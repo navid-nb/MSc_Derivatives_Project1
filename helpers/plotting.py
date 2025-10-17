2047b8f057fa9aba1eb43b48b3336220ef7a7642
navid-nb <navid.namazib@gmail.com>
2025-10-17 19:18:47 -0400

    updated

---
+++ helpers/plotting.py
@@ -0,0 +1,108 @@
+import matplotlib.pyplot as plt
+import matplotlib.transforms as transforms
+import datetime as dt
+import pandas as pd
+
+def Q3_plot1(otm_options: pd.DataFrame) -> plt.Figure:
+    """Plot implied volatility smiles for OTM options on Jan 17, 2020 and Mar 20, 2020."""
+    moneyness_col='moneyness'
+    options_jan17 = otm_options[otm_options['date'] == dt.date(2020,1,17)]
+    options_mar20 = otm_options[otm_options['date'] == dt.date(2020,3,20)]
+
+    maturities_jan17 = [dt.date(2020,2,14), dt.date(2020,7,17)]
+    maturities_mar20 = [dt.date(2020,4,17), dt.date(2020,10,16)]
+
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+
+    # Left panel: Jan 17 options
+    ax = axes[0]
+    for exdate in maturities_jan17:
+        df = options_jan17[options_jan17['exdate'] == exdate]
+        ax.scatter(df[moneyness_col], df['self_calc_imp_vol_BMS'], s=20, alpha=0.6, label=f'Self Calc IV exp:{exdate}')
+        df_sorted = df.sort_values(moneyness_col)
+        ax.plot(df_sorted[moneyness_col], df_sorted['implied_vol_bms'], label=f'Provider IV exp:{exdate}')
+    ax.set_title('Options quoted on Jan 17, 2020')
+    ax.set_xlabel('Moneyness (K / S_t)')
+    ax.set_ylabel('Implied Volatility')
+    ax.grid(True)
+    ax.legend()
+    # Add horizontal arrow below x-axis for left subplot
+    trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
+    ax.annotate('', xy=(1.4, -0.16), xytext=(1.01, -0.16),
+                xycoords=trans,
+                arrowprops=dict(arrowstyle='->', color='green', lw=1.5))
+    ax.text(1.2, -0.18, 'OTM Calls', transform=trans,
+            ha='center', va='top', fontsize=10, color='red')
+    ax.annotate('', xy=(0.6, -0.16), xytext=(0.99, -0.16),
+                xycoords=trans,
+                arrowprops=dict(arrowstyle='->', color='green', lw=1.5))
+    ax.text(0.8, -0.18, 'OTM Puts', transform=trans,
+            ha='center', va='top', fontsize=10, color='red')
+
+    # Right panel: Mar 20 options
+    ax = axes[1]
+    for exdate in maturities_mar20:
+        df = options_mar20[options_mar20['exdate'] == exdate]
+        ax.scatter(df[moneyness_col], df['self_calc_imp_vol_BMS'], s=20, alpha=0.6, label=f'Self Calc IV exp:{exdate}')
+        df_sorted = df.sort_values(moneyness_col)
+        ax.plot(df_sorted[moneyness_col], df_sorted['implied_vol_bms'], label=f'Provider IV exp:{exdate}')
+    ax.set_title('Options quoted on Mar 20, 2020')
+    ax.set_xlabel('Moneyness (K / S_t)')
+    ax.set_ylabel('Implied Volatility')
+    ax.grid(True)
+    ax.legend()
+    # Add horizontal arrow below x-axis for left subplot
+    trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
+    ax.annotate('', xy=(1.4, -0.16), xytext=(1.01, -0.16),
+                xycoords=trans,
+                arrowprops=dict(arrowstyle='->', color='green', lw=1.5))
+    ax.text(1.2, -0.18, 'OTM Calls', transform=trans,
+            ha='center', va='top', fontsize=10, color='red')
+    ax.annotate('', xy=(0.6, -0.16), xytext=(0.99, -0.16),
+                xycoords=trans,
+                arrowprops=dict(arrowstyle='->', color='green', lw=1.5))
+    ax.text(0.8, -0.18, 'OTM Puts', transform=trans,
+            ha='center', va='top', fontsize=10, color='red')
+
+    plt.suptitle('Implied Volatility Smiles for OTM Options')
+    return fig
+
+def Q3_plot2(otm_options: pd.DataFrame) -> plt.Figure:
+    """Plot implied volatility smiles for OTM options on Jan 17, 2020 and Mar 20, 2020."""
+    moneyness_col='moneyness_adjusted'
+    options_jan17 = otm_options[otm_options['date'] == dt.date(2020,1,17)]
+    options_mar20 = otm_options[otm_options['date'] == dt.date(2020,3,20)]
+
+    maturities_jan17 = [dt.date(2020,2,14), dt.date(2020,7,17)]
+    maturities_mar20 = [dt.date(2020,4,17), dt.date(2020,10,16)]
+
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+
+    # Left panel: Jan 17 options
+    ax = axes[0]
+    for exdate in maturities_jan17:
+        df = options_jan17[options_jan17['exdate'] == exdate]
+        ax.scatter(df[moneyness_col], df['self_calc_imp_vol_BMS'], s=20, alpha=0.6, label=f'Self Calc IV exp:{exdate}')
+        df_sorted = df.sort_values(moneyness_col)
+        ax.plot(df_sorted[moneyness_col], df_sorted['implied_vol_bms'], label=f'Provider IV exp:{exdate}')
+    ax.set_title('Options quoted on Jan 17, 2020')
+    ax.set_xlabel(r'Standardized Moneyness $= \ln(K/S) / (\sigma\sqrt{\tau})$')
+    ax.set_ylabel('Implied Volatility')
+    ax.grid(True)
+    ax.legend()
+
+    # Right panel: Mar 20 options
+    ax = axes[1]
+    for exdate in maturities_mar20:
+        df = options_mar20[options_mar20['exdate'] == exdate]
+        ax.scatter(df[moneyness_col], df['self_calc_imp_vol_BMS'], s=20, alpha=0.6, label=f'Self Calc IV exp:{exdate}')
+        df_sorted = df.sort_values(moneyness_col)
+        ax.plot(df_sorted[moneyness_col], df_sorted['implied_vol_bms'], label=f'Provider IV exp:{exdate}')
+    ax.set_title('Options quoted on Mar 20, 2020')
+    ax.set_xlabel(r'Standardized Moneyness $= \ln(K/S) / (\sigma\sqrt{\tau})$')
+    ax.set_ylabel('Implied Volatility')
+    ax.grid(True)
+    ax.legend()
+
+    plt.suptitle('Implied Volatility Smiles for OTM Options with Adjusted Moneyness')
+    return fig
